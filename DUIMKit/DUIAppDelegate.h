a521976499f441351fe117aa0f4bed9dbca572ce
Angel Garcia <angelgarcia.mail@gmail.com>
2014-03-28 11:16:47 +0100

    Initial Commit

---
+++ DUIMKit/DUIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DUIAppDelegate.h
+//  DUIMKit
+//
+//  Created by Angel Garcia on 28/03/14.
+//  Copyright (c) 2014 DUIMKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DUIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DUIMKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DUIMKit
+//
+//  Created by Angel Garcia on 28/03/14.
+//  Copyright (c) 2014 DUIMKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DUIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DUIAppDelegate class]));
+    }
+}
